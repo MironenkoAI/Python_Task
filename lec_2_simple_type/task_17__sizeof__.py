86adb9c0dfd5332b3009c7fee451265d183f4859
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-18 23:36:48 +0300

    метод __sizeof__

---
+++ lec_2_simple_type/task_17__sizeof__.py
@@ -0,0 +1,11 @@
+# метод __sizeof__ возвращает размер объекта
+
+empty_str = ''
+en_str = 'Text'
+ru_str = 'Текст'
+unicode_str = '🎎😍🛩🎧'
+
+print(empty_str.__sizeof__())
+print(en_str.__sizeof__())
+print(ru_str.__sizeof__())
+print(unicode_str.__sizeof__())
