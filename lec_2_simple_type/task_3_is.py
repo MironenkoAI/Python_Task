ceaa8eaafb810510f402b10b357342ad853477cc
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-18 17:55:33 +0300

    оператор is

---
+++ lec_2_simple_type/task_3_is.py
@@ -0,0 +1,7 @@
+# is - оператор сравнивает переменные по типу
+
+num = 2 + 2 * 2    # 6
+digit = 36 / 6     # 6.0
+print(num, digit)    # 6  6.0
+print(num == digit)  # сравнивает по значению True 
+print(num is digit)  # сравнивает по типу False
