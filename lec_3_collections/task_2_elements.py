6fe4f28b0c2d50278cecd23585ad52c839a0b47a
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-22 14:03:21 +0300

    метод insert для list

--- lec_3_collections/task_2_elements.py
+++ lec_3_collections/task_2_elements.py
@@ -1,7 +1,7 @@
 my_list = [2, 4, 6, 8, 10, 12]
 
 print(my_list[0]) # индексация начинается с 0
-print(my_list[6]) # IndexError: list index out of range
+#print(my_list[6]) # IndexError: list index out of range
 
 print(my_list[-1]) # можно обратиться к индексам с конца, добавив минус
                    # индексация тогда будет с 1
