893282ca40baa5cfda4a3d3c5fe0627bc8455724
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-22 13:27:07 +0300

    метод pop для list

---
+++ lec_3_collections/task_5_pop.py
@@ -0,0 +1,8 @@
+my_list = [2, 4, 6, 8, 10, 12]
+spam = my_list.pop() # без пердачи значения удалит последний элемент из списка
+                     # и сохранит его в переменную spam
+print(spam, my_list)   # 12 [2, 4, 6, 8, 10]
+
+eggs = my_list.pop(1) # удалит заданный элемент и также сохранит в переменную
+print(eggs, my_list)  # 4 [2, 6, 8, 10]
+err = my_list.pop(10) # IndexError: pop index out of range
