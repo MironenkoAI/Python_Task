7f04d6ae11ae56790e992e25f50c6e621a670462
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-24 18:04:07 +0300

    метод setdefault для dict

---
+++ lec_3_collections/task_24_setdefault.py
@@ -0,0 +1,14 @@
+# Метод setdefault возвращает значение и добавляет ключ в словарь 
+my_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'ten': 10}
+
+spam = my_dict.setdefault('five') # создаст новый ключ в словаре со значением None 
+print(f'{spam = }\t{my_dict = }')
+
+eggs = my_dict.setdefault('six', 6) # создаст новый ключ в словаре со значением 6
+print(f'{eggs = }\t{my_dict = }')
+
+new_spam = my_dict.setdefault('two') # выдаст значение по найденному ключу
+print(f'{new_spam = }\t{my_dict = }')
+
+new_eggs = my_dict.setdefault('one', 1_000) # выдаст значение по найденному ключу, 1_000 добавится если ключ не найден
+print(f'{new_eggs = }\t{my_dict = }')
