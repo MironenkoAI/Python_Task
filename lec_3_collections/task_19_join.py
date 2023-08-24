d41617c5515e26d771aac4218a48a97c4b24e626
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-24 14:50:56 +0300

    метод join для строк

---
+++ lec_3_collections/task_19_join.py
@@ -0,0 +1,4 @@
+data = ['https:', '', 'habr.com', 'ru', 'users', 'dzhoker1', 'posts']
+url = '/'.join(data) # склеит текст через заданный символ/текст
+print(url)  # https://habr.com/ru/users/dzhoker1/posts
+            # в начале и в конце символ не добавляется
