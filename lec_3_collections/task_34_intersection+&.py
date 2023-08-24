6beb88691eada8535cbcb5c885939df52dfe14f0
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-24 22:01:26 +0300

    способы пересечения множеств set

---
+++ lec_3_collections/task_34_intersection+&.py
@@ -0,0 +1,10 @@
+# метод intersection и & для пересечения множеств
+my_set = {3, 4, 2, 5, 6, 1, 7}
+other_set = {1, 4, 42, 314}
+new_set = my_set.intersection(other_set) # найдет общие элементы множеств
+print(f'{my_set = }\n{other_set = }\n{new_set = }')
+
+my_set = {3, 4, 2, 5, 6, 1, 7}
+other_set = {1, 4, 42, 314}
+new_set = my_set & other_set   # новый способ
+print(f'{my_set = }\n{other_set = }\n{new_set = }')
