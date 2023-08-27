3c0d875d173972f8be7f605736ebb691fab9dbff
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-27 18:39:47 +0300

    Функция next

---
+++ lec_5_iterators+generators/task_4_next.py
@@ -0,0 +1,20 @@
+""" Функция next
+Функция next имеет формат next(iterator[, default]). На вход функция принимает
+итератор, который вернула функция iter. Каждый вызов функции возвращает
+очередной элемент итератора."""
+data = [2, 4, 6, 8]
+list_iter = iter(data)
+print(next(list_iter))
+print(next(list_iter))
+print(next(list_iter))
+print(next(list_iter))
+# print(next(list_iter)) # StopIteration
+
+data = [2, 4, 6, 8]
+list_iter = iter(data)
+print(next(list_iter, 42)) # когда закончится последовательность, 
+print(next(list_iter, 42)) # будет возвращать 42
+print(next(list_iter, 42))
+print(next(list_iter, 42))
+print(next(list_iter, 42))
+print(next(list_iter, 42))
--- lec_5_iterators+generators/task_4_sentinel.py
+++
@@ -1,9 +0,0 @@
-data = [2, 4, 6, 8]
-# list_iter = iter(data, 6) # TypeError: iter(v, w): v must be callable
-
-import functools
-
-f = open('mydata.bin', 'rb') # должен быть сторонний файл mydata.bin
-for block in iter(functools.partial(f.read, 16), b''):
-    print(block)
-f.close()
