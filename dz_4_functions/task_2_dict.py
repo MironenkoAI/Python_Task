c220bb423bd1eb75277a7217491a073158e88122
Anastasiya Mironenko <nastya_911@mail.ru>
2023-08-27 14:22:46 +0300

    решение задачи работа со словарем

--- dz_4_functions/task_2_dict.py
+++ dz_4_functions/task_2_dict.py
@@ -2,3 +2,11 @@
 и возвращающую словарь, где ключ — значение переданного аргумента, 
 а значение — имя аргумента. Если ключ не хешируем, используйте его 
 строковое представление."""
+
+
+def reverse_dict(**kwargs):
+    return {value if isinstance(value, (int, float, str, tuple)) 
+            else str(value): key for key, value in kwargs.items()}
+
+
+print(reverse_dict(a=5, b=[2,3], c='Hello', d=(4,5)))
