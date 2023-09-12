91fb8e7a0baaeb7ede8c5f1b9b8178a6997839cd
Anastasiya Mironenko <nastya_911@mail.ru>
2023-09-12 10:02:16 +0300

    создание собственного модуля и правила его оформления

--- lec_6_modules/task_3_as.py
+++ lec_6_modules/task_3_as.py
@@ -4,5 +4,5 @@ from sys import builtin_module_names as bmn, path as p
 print(bmn)
 print(*p, sep='\n')
 print(rnd.randint(1, 6))
-print(path) # NameError: name 'path' is not defined
-print(sys.path) # NameError: name 'sys' is not defined
+# print(path) # NameError: name 'path' is not defined
+# print(sys.path) # NameError: name 'sys' is not defined
---
+++ lec_6_modules/task_6_start_my_module.py
@@ -0,0 +1,8 @@
+import task_5_my_module
+
+x = task_5_my_module.mul # Плохой приём
+y = task_5_my_module._START_MULT # Очень плохой приём
+z = task_5_my_module.sub(73, 42)
+print(x(2, 3))
+print(y)
+print(z)
