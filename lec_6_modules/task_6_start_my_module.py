3fc7f59e35da99ff37afa9a2a9cb07d746392760
Anastasiya Mironenko <nastya_911@mail.ru>
2023-09-12 10:24:56 +0300

    создание Пакетов, импорт Пакетов, обращение к функциям в файлах Пакета

--- lec_6_modules/task_6_start_my_module.py
+++ lec_6_modules/task_6_start_my_module.py
@@ -1,8 +1,14 @@
-import task_5_my_module
+from mathematical import task_5_my_module_math as base_math
+from mathematical.task_7_my_module_math2 import exp
 
-x = task_5_my_module.mul # Плохой приём
-y = task_5_my_module._START_MULT # Очень плохой приём
-z = task_5_my_module.sub(73, 42)
+x = base_math.mul # Плохой приём
+y = base_math._START_MULT # Очень плохой приём
+z = base_math.sub(73, 42)
 print(x(2, 3))
 print(y)
+print(z)
+
+x = base_math.div(12, 5)
+z = exp(2, 3)
+print(x)
 print(z)
